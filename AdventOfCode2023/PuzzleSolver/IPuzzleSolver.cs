c6dbffc8fd7158188bfa968d9a2c39a50068ed14
Carl Hirschman <carlhi@microsoft.com>
2023-12-01 12:21:22 -0800

    Initial commit.

---
+++ AdventOfCode2023/PuzzleSolver/IPuzzleSolver.cs
@@ -0,0 +1,9 @@
+﻿namespace AdventOfCode2023.PuzzleSolver
+{
+    internal interface IPuzzleSolver
+    {
+        string SolvePartOne(bool test = false);
+
+        string SolvePartTwo(bool test = false);
+    }
+}
---
+++ AdventOfCode2023/Utilities/PuzzleReader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace AdventOfCode2023.Utilities
+{
+    internal static class PuzzleReader
+    {
+        public static IEnumerable<string> ReadLines(int day, bool isTest = false)
+        {
+            string fileName = isTest ? $"Dec{day}_test.txt" : $"Dec{day}.txt";
+            using (var stream = new FileStream($@"c:\docs\AdventOfCode2022\{fileName}", FileMode.Open))
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        yield return reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
