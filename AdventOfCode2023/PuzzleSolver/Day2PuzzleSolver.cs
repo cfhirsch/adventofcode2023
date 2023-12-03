32c99de6bb91a65f65ea85872335b835f97ca9c9
Carl Hirschman <carlhi@microsoft.com>
2023-12-02 16:23:00 -0800

    Solve Dec 2 Part 2

--- AdventOfCode2023/PuzzleSolver/Day2PuzzleSolver.cs
+++ AdventOfCode2023/PuzzleSolver/Day2PuzzleSolver.cs
@@ -81,7 +81,61 @@ namespace AdventOfCode2023.PuzzleSolver
 
         public string SolvePartTwo(bool test = false)
         {
-            throw new NotImplementedException();
+            int sum = 0;
+            foreach (string line in PuzzleReader.ReadLines(2, test))
+            {
+                string game = line.Substring("Game 1: ".Length);
+                string[] turns = game.Split(';');
+
+                int minRed = 0;
+                int minGreen = 0;
+                int minBlue = 0;
+
+                foreach (string turn in turns)
+                {
+                    string[] samples = turn.Split(',');
+                    foreach (string sample in samples)
+                    {
+                        Match match = sampleReg.Match(sample);
+                        int numCubes = Int32.Parse(match.Groups[1].Value);
+                        string color = match.Groups[2].Value;
+
+                        switch (color)
+                        {
+                            case "red":
+                                if (numCubes > minRed)
+                                {
+                                    minRed = numCubes;
+                                }
+
+                                break;
+
+                            case "green":
+                                if (numCubes > minGreen)
+                                {
+                                    minGreen = numCubes;
+                                }
+
+                                break;
+
+                            case "blue":
+                                if (numCubes > minBlue)
+                                {
+                                    minBlue = numCubes;
+                                }
+
+                                break;
+
+                            default:
+                                throw new ArgumentException($"Unexpected color {color}.");
+                        }
+                    }
+                }
+
+                sum += (minRed * minGreen * minBlue);
+            }
+
+            return sum.ToString();
         }
     }
 }
--- README.md
+++ README.md
@@ -8,3 +8,8 @@ Part 1 was a straightforward scan through each input line to look for digits.
 
 Solving Part 2 entailed scanning through the line to look for words for digits, in addition to digits. One gotcha was to be mindful of 
 overlap between digit words. For example, "twone" contains 2 AND 1 and both digits need to be added to the list.
+
+- Dec 2
+
+Solving both parts was straightforward; split each line into the list of samples, use a regex to determine number and color of blocks,
+then apply whatever logic was applicable to each part.
