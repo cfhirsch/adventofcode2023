d88eb8df3bfc8e2ac27b4beb4927c07579a75683
Carl Hirschman <carlhi@microsoft.com>
2023-12-19 17:07:18 -0800

    Save work towards solving Day 19 Part 2.

--- AdventOfCode2023/PuzzleSolver/Day19PuzzleSolver.cs
+++ AdventOfCode2023/PuzzleSolver/Day19PuzzleSolver.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics.Eventing.Reader;
-using System.Drawing.Text;
+using System.Data;
 using System.Linq;
 using System.Text.RegularExpressions;
 using AdventOfCode2023.Utilities;
@@ -10,10 +9,10 @@ namespace AdventOfCode2023.PuzzleSolver
 {
     internal class Day19PuzzleSolver : IPuzzleSolver
     {
+        private static Regex compReg = new Regex(@"(\w+)([<>])(\d+):(\w+)", RegexOptions.Compiled);
+
         public string SolvePartOne(bool test = false)
         {
-            var compReg = new Regex(@"(\w+)([<>])(\d+):(\w+)", RegexOptions.Compiled);
-
             var workflows = new Dictionary<string, string>();
             var ratings = new List<string>();
 
@@ -30,7 +29,7 @@ namespace AdventOfCode2023.PuzzleSolver
                 {
                     int bracketPos = line.IndexOf("{");
                     workflows.Add(line.Substring(0, bracketPos),
-                                  line.Substring(bracketPos + 1, line.Length - bracketPos - 2)); 
+                                  line.Substring(bracketPos + 1, line.Length - bracketPos - 2));
                 }
                 else
                 {
@@ -128,7 +127,264 @@ namespace AdventOfCode2023.PuzzleSolver
 
         public string SolvePartTwo(bool test = false)
         {
-            throw new NotImplementedException();
+            var compReg = new Regex(@"(\w+)([<>])(\d+):(\w+)", RegexOptions.Compiled);
+
+            var workflows = new Dictionary<string, string>();
+
+            foreach (string line in PuzzleReader.ReadLines(19, test))
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+                    break;
+                }
+
+                int bracketPos = line.IndexOf("{");
+                workflows.Add(line.Substring(0, bracketPos),
+                              line.Substring(bracketPos + 1, line.Length - bracketPos - 2));
+            }
+
+            // Let's try looking at this as a binary tree. The key in each node is an expression.
+            // The left node corresponds to the expression being true, the other to its being false.
+            // "A" and "R" are leaf nodes. We'll form the union of sets corresponding to "A" leaf nodes.
+            string workflowKey = "in";
+
+            var expression = WorkflowToExpression(workflowKey, workflows);
+
+            var baseSet = new Dictionary<string, HashSet<int>>();
+            baseSet["x"] = Enumerable.Range(1, 4000).ToHashSet();
+            baseSet["m"] = Enumerable.Range(1, 4000).ToHashSet();
+            baseSet["a"] = Enumerable.Range(1, 4000).ToHashSet();
+            baseSet["s"] = Enumerable.Range(1, 4000).ToHashSet();
+
+            var finalSet = expression.NumThatSatisfy(baseSet);
+
+            long result = 1;
+            foreach (HashSet<int> set in finalSet.Values)
+            {
+                result *= set.Count;
+            }
+
+            return result.ToString();
         }
+
+        private Expression WorkflowToExpression(
+            string workflowKey,
+            Dictionary<string, string> workFlows)
+        {
+            string[] conditions = workFlows[workflowKey].Split(',');
+            Expression prev = null;
+            Expression current = null;
+            foreach (string condition in conditions)
+            {
+                Match match = compReg.Match(condition);
+                if (match.Success)
+                {
+                    string category = match.Groups[1].Value;
+                    string comp = match.Groups[2].Value;
+                    int val = Int32.Parse(match.Groups[3].Value);
+                    string label = match.Groups[4].Value;
+
+                    current = ComparisonRuleToExpression(category, comp, val, label, workFlows);
+                }
+                else
+                {
+                    switch (condition)
+                    {
+                        case "R":
+                            current = new FalseExpression();
+                            break;
+
+                        case "A":
+                            current = new TrueExpression();
+                            break;
+
+                        default:
+                            current = WorkflowToExpression(condition, workFlows);
+                            break;
+                    }
+                }
+
+                if (prev != null)
+                {
+                    var temp = new CompoundExpression
+                    {
+                        Left = prev,
+                        LogicalOperator = LogicalOperator.Or,
+                        Right = current
+                    };
+
+                    current = temp;
+                }
+
+                prev = current;
+            }
+
+            return current;
+        }
+
+        private Expression ComparisonRuleToExpression(
+            string category,
+            string comp,
+            int value,
+            string label,
+            Dictionary<string, string> workFlows)
+        {
+            var compoundExpression = new CompoundExpression();
+            var expression = new ComparisonExpression { Left = category, Value = value };
+            compoundExpression.Left = expression;
+            compoundExpression.LogicalOperator = LogicalOperator.And;
+
+            switch (comp)
+            {
+                case ">":
+                    expression.Comparator = Comparator.GreaterThan;
+                    break;
+
+                case "<":
+                    expression.Comparator = Comparator.LessThan;
+                    break;
+
+                default:
+                    throw new ArgumentException($"Unexpected comparator {comp}.");
+            }
+
+            switch (label)
+            {
+                case "R":
+                    compoundExpression.Right = new FalseExpression();
+                    break;
+
+                case "A":
+                    compoundExpression.Right = new TrueExpression();
+                    break;
+
+                default:
+                    compoundExpression.Right = WorkflowToExpression(label, workFlows);
+                    break;
+            }
+
+            return compoundExpression;
+        }
+    }
+
+    internal abstract class Expression
+    {
+        public abstract Dictionary<string, HashSet<int>> NumThatSatisfy(Dictionary<string, HashSet<int>> sets);
+    }
+
+    internal class ComparisonExpression : Expression
+    {
+        public string Left { get; set; }
+
+        public Comparator Comparator { get; set; }
+
+        public long Value { get; set; }
+
+        public override Dictionary<string, HashSet<int>> NumThatSatisfy(Dictionary<string, HashSet<int>> sets)
+        {
+            var newSets = new Dictionary<string, HashSet<int>>();
+
+            foreach (KeyValuePair<string, HashSet<int>> kvp in sets)
+            {
+                newSets.Add(kvp.Key, kvp.Value.ToHashSet());
+            }
+
+            switch (this.Comparator)
+            {
+                case Comparator.LessThan:
+                    newSets[this.Left] = newSets[this.Left].Where(x => x < this.Value).ToHashSet();
+                    break;
+
+                case Comparator.LessThanOrEqualTo:
+                    newSets[this.Left] = newSets[this.Left].Where(x => x >= this.Value).ToHashSet();
+                    break;
+
+                case Comparator.GreaterThan:
+                    newSets[this.Left] = newSets[this.Left].Where(x => x > this.Value).ToHashSet();
+                    break;
+
+                case Comparator.GreaterThanOrEqualTo:
+                    newSets[this.Left] = newSets[this.Left].Where(x => x >= this.Value).ToHashSet();
+                    break;
+
+                default:
+                    throw new ArgumentException($"Unexpected compartor {this.Comparator}.");
+            }
+
+            return newSets;
+        }
+    }
+
+    internal class CompoundExpression : Expression
+    {
+        public Expression Left { get; set; }
+
+        public LogicalOperator LogicalOperator { get; set; }
+
+        public Expression Right { get; set; }
+
+        public override Dictionary<string, HashSet<int>> NumThatSatisfy(Dictionary<string, HashSet<int>> sets)
+        {
+            Dictionary<string, HashSet<int>> left = this.Left.NumThatSatisfy(sets);
+            Dictionary<string, HashSet<int>> right = this.Right.NumThatSatisfy(sets);
+
+            var newResult = new Dictionary<string, HashSet<int>>();
+            foreach (string key in sets.Keys)
+            {
+                switch (this.LogicalOperator)
+                {
+                    case LogicalOperator.And:
+                        newResult[key] = left[key].Intersect(right[key]).ToHashSet();
+                        break;
+
+                    case LogicalOperator.Or:
+                        newResult[key] = left[key].Union(right[key]).ToHashSet();
+                        break;
+
+                    default:
+                        throw new ArgumentException($"Unexpected operator {this.LogicalOperator}");
+                }
+            }
+
+            return newResult;
+        }
+    }
+
+    internal class FalseExpression : Expression
+    {
+        public override Dictionary<string, HashSet<int>> NumThatSatisfy(Dictionary<string, HashSet<int>> sets)
+        {
+            return sets.Keys.ToDictionary(x => x, x => new HashSet<int>());
+        }
+    }
+
+    internal class TrueExpression : Expression
+    {
+        public override Dictionary<string, HashSet<int>> NumThatSatisfy(Dictionary<string, HashSet<int>> sets)
+        {
+            return sets.ToDictionary(kvp => kvp.Key, kvp => kvp.Value.ToHashSet());
+        }
+    }
+
+    internal enum LogicalOperator
+    {
+        None,
+
+        And,
+
+        Or
+    }
+
+    internal enum Comparator
+    {
+        None,
+
+        LessThan,
+
+        GreaterThan,
+
+        LessThanOrEqualTo,
+
+        GreaterThanOrEqualTo
     }
 }
